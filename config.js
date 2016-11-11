c4c71eceda71a5add33b50ffa5ac316c2c6e0810
jandginvestment <jandginvestment@gmail.com>
2016-11-11 22:48:57 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
     
     'secret':'whatshouldido',
-    'database':'mongodb://juliusmas:amj54321@ds047901.mlab.com:47901/ecomm'
+    'database':'mongodb://juliusmasi908:password@ds047901.mlab.com:47901/ecomm'
 
 }
